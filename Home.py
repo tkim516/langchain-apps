7819cde54691dbf64b4bd3c9fcb1f40135e32afb
Tyler Kim <tylerk516@gmail.com>
2025-01-03 22:28:43 -0800

    text changes

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from functions.check_api import check_openai_api_key
 
 # Page configuration
-st.set_page_config(page_title="Tyler's LangChain Apps", 
+st.set_page_config(page_title="Practice LangChain Apps", 
                    page_icon="",
                    layout="wide")
 
@@ -18,8 +18,12 @@ pinecone_api_key = os.environ.get('PINECONE_API_KEY', '')
 if not pinecone_api_key:
     raise ValueError("PINECONE_API_KEY environment variable is not set.")
 
-st.header('Research Paper Assistant')
-st.subheader('Upload a PDF and ask questions about the paper. Built with LangChain and Pinecone')
 
-st.header('YouTube Assisstant')
-st.subheader('Copy a YouTube video link and ask questions about the video. The video transcript is embedded and loaded into the Pinecone database.')
+st.subheader('Gift Idea Assistant')
+st.write('Generate gift ideas based on inputs.')
+
+st.subheader('Research Paper Assistant')
+st.write('Upload a PDF and ask questions about the paper.')
+
+st.subheader('YouTube Assisstant')
+st.write('Copy a YouTube video link and ask questions about the video.')
