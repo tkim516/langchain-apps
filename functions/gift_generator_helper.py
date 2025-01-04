5a44af859d035bafbbd4ac4627e2c09a00947654
Tyler Kim <tylerk516@gmail.com>
2025-01-04 14:48:12 -0800

    added search functionality to gift generator using serper

--- functions/gift_generator_helper.py
+++ functions/gift_generator_helper.py
@@ -2,37 +2,38 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 import os
 
-def generate_gifts(recipient_hobbies, recipient_age, relationship, occasion, budget, api_key):
-    """
-    Generate gift suggestions using LangChain and OpenAI.
+#from langgraph.graph import MessagesState, StateGraph
+from langchain_core.tools import tool
 
-    Args:
-        recipient_hobbies (str): Hobbies of the recipient.
-        recipient_age (int): Age of the recipient.
-        relationship (str): Relationship to the recipient.
-        occasion (str): Occasion for the gift.
-        budget (str): Budget range in USD.
-        api_key (str): The API key for OpenAI.
+from langchain_core.messages import SystemMessage
+#from langgraph.prebuilt import ToolNode
 
-    Returns:
-        str: Generated gift suggestions.
-    """
+from langchain_core.tools import tool
+import requests
+import json
+
+#graph_builder = StateGraph(MessagesState)
+
+# Build a tool which can search the internet for gifts
+# Generate a query to pass to search based on the user's interests
+
+
+def generate_gift_search_query(recipient_hobbies, recipient_age, budget, api_key):
+    """Generate gift search queries based on interests."""
+    
     llm = ChatOpenAI(model="gpt-4", temperature=0.5, openai_api_key=os.environ.get("OPENAI_API_KEY"))
     
     prompt = PromptTemplate(
         template="""
-        Using the details provided, generate a thoughtful list of gift suggestions that are well-suited to the individual’s interests, age, 
-        the nature of your relationship with them, the occasion, and the given budget range. For each gift idea, include a brief explanation 
-        of why it would be a suitable choice. Aim for unique, meaningful, and practical suggestions rather than generic options.
+        Using the details provided, generate a Google search shopping query for gifts that are well-suited to the individual’s interests, age, 
+        the nature of your relationship with them, the occasion, and the given budget range.
 
         Person's interests: {recipient_hobbies}
         Person's age in years: {recipient_age}
-        Gift giver's relationship to the recipient: {relationship}
-        Occasion: {occasion}
         Budget range in USD: {budget}
         """,
         input_variables=[
-            "recipient_hobbies", "recipient_age", "relationship", "occasion", "budget"
+            "recipient_hobbies", "recipient_age", "budget"
         ]
     )
 
@@ -40,11 +41,27 @@ def generate_gifts(recipient_hobbies, recipient_age, relationship, occasion, bud
     message = prompt.format(
         recipient_hobbies=recipient_hobbies,
         recipient_age=recipient_age,
-        relationship=relationship,
-        occasion=occasion,
         budget=budget,
     )
     
     # Get the response
     response = llm(message)
     return response
+
+def serper_shopping_search(serper_api_key: str, query: str) -> dict:
+  """Search internet for gifts related to query"""
+
+  url = "https://google.serper.dev/shopping"
+
+  payload = json.dumps({
+    "q": query
+  })
+  headers = {
+    'X-API-KEY': serper_api_key,
+    'Content-Type': 'application/json'
+  }
+  response = requests.request("POST", url, headers=headers, data=payload)
+
+  print(response.text)
+
+  return response
--- pages/Gift_Idea_Assistant.py
+++ pages/Gift_Idea_Assistant.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import functions.gift_generator_helper as lch
 from functions.check_api import check_openai_api_key
+from langchain_community.utilities import GoogleSerperAPIWrapper
+import os
+
 
 st.set_page_config(page_title="Gift Idea Assistant", 
                    page_icon="🎁",
@@ -11,18 +14,36 @@ st.markdown("<h1 style='text-align: center; color: #57cfff;'>Gift Idea Assistant
 check_openai_api_key(st.session_state)
 api_key = st.session_state['OPENAI_API_KEY']
 
+serper_api_key = os.environ.get('SERPER_API_KEY', '')
+if not serper_api_key:
+    raise ValueError("SERPER_API_KEY environment variable is not set.")
+
+
+
 with st.sidebar.form("input_form"):
     recipient_hobbies = st.text_input("Recipient's Hobbies", placeholder="Reading, Cooking")
     recipient_age = st.number_input("Recipient's Age", min_value=0, max_value=120, value=20, step=1)
-    relationship = st.text_input("Your Relationship to the Recipient", placeholder="Friend, Family Member")
-    occasion = st.text_input("Occasion", placeholder="Birthday, Anniversary")
     budget = st.text_input("Budget (USD)", placeholder="$50-$100")
     submit_button = st.form_submit_button('Generate Gift Suggestions')
 
 if submit_button:
   try:
-    response = lch.generate_gifts(recipient_hobbies, recipient_age, relationship, occasion, budget, api_key)
-    st.success("Gift Suggestions:")
-    st.write(response.content)
+    response = lch.generate_gift_search_query(recipient_hobbies, recipient_age, budget, api_key)
+    st.subheader("Gift Suggestions")
+
+    search_result = lch.serper_shopping_search(serper_api_key, response.content).json()
+    products = search_result["shopping"]
+    first_five_products = products[:5]
+
+    for product in first_five_products:
+      title = product.get('title', 'No title available')
+      link = product.get('link', 'No link available')
+      price = product.get('price', 'No price available')
+      
+      st.write(f"Title: {title}")
+      st.write(f"Link: {link}")
+      st.write(f"Price: {price}")
+      st.write('---')
+
   except Exception as e:
     st.error(f"An error occurred: {e}")
