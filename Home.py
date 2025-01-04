65a0e43be69d35ee9d6e4bb5a850809257e2410f
Tyler Kim <tylerk516@gmail.com>
2025-01-04 15:01:42 -0800

    text changes on Home.py

--- Home.py
+++ Home.py
@@ -18,12 +18,23 @@ pinecone_api_key = os.environ.get('PINECONE_API_KEY', '')
 if not pinecone_api_key:
     raise ValueError("PINECONE_API_KEY environment variable is not set.")
 
+st.subheader('About')
+st.write('''
+This app showcases a collection of small, practice applications built with LangChain, OpenAI, Serper API, Pinecone, and Streamlit. 
+- **LangChain**: Provides the logic for connecting and orchestrating applications.  
+- **OpenAI**: Utilizes `gpt-4o-mini` for the LLM and `text-embedding-3-large` for embeddings.  
+- **Pinecone**: Handles vector embedding storage and search.  
+- **Serper API**: Enables internet search functionality.  
+- **Streamlit**: Serves as the framework for building the web app.
+''')
+
 
 st.subheader('Gift Idea Assistant')
-st.write('Generate gift ideas based on inputs.')
+st.write('Find unique gift ideas by searching the internet using the Serper API.')
 
 st.subheader('Research Paper Assistant')
-st.write('Upload a PDF and ask questions about the paper.')
+st.write('Upload a PDF to explore and ask detailed questions about its content.')
 
-st.subheader('YouTube Assisstant')
-st.write('Copy a YouTube video link and ask questions about the video.')
+st.subheader('YouTube Assistant')
+st.write('Paste a YouTube video link to ask questions and gain insights from the video.')
+
